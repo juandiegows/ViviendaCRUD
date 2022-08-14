0312ce4cd1b7e5a06abcc2b0ea947ceaec685eec
Natalia Quintero <natalyquinter28@gmail.com>
2022-08-14 14:53:42 -0500

    Se agregó el modelo TipoVivienda y Vivienda

---
+++ src/Modelo/TipoViviendaDAO.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Natalia
+ */
+public class TipoViviendaDAO {
+    private int ID;
+    private String Nombre;
+
+    public TipoViviendaDAO() {
+    }
+
+    public TipoViviendaDAO(int ID, String Nombre) {
+        this.ID = ID;
+        this.Nombre = Nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+    
+    
+}
---
+++ src/Modelo/ViviendaDAO.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Natalia
+ */
+public class ViviendaDAO {
+    private int Escritura;
+    private int TipoViviendaID;
+    private TipoViviendaDAO TipoVivienda;
+    private String Direccion;
+    private int MunicipioID;
+    private MunicipioDAO Municipio;
+
+    public ViviendaDAO() {
+    }
+
+    public ViviendaDAO(int Escritura, int TipoViviendaID, String Direccion, int MunicipioID) {
+        this.Escritura = Escritura;
+        this.TipoViviendaID = TipoViviendaID;
+        this.Direccion = Direccion;
+        this.MunicipioID = MunicipioID;
+    }
+
+    public ViviendaDAO(int Escritura, int TipoViviendaID, TipoViviendaDAO TipoVivienda, String Direccion, int MunicipioID, MunicipioDAO Municipio) {
+        this.Escritura = Escritura;
+        this.TipoViviendaID = TipoViviendaID;
+        this.TipoVivienda = TipoVivienda;
+        this.Direccion = Direccion;
+        this.MunicipioID = MunicipioID;
+        this.Municipio = Municipio;
+    }
+
+    public int getEscritura() {
+        return Escritura;
+    }
+
+    public void setEscritura(int Escritura) {
+        this.Escritura = Escritura;
+    }
+
+    public int getTipoViviendaID() {
+        return TipoViviendaID;
+    }
+
+    public void setTipoViviendaID(int TipoViviendaID) {
+        this.TipoViviendaID = TipoViviendaID;
+    }
+
+    public TipoViviendaDAO getTipoVivienda() {
+        return TipoVivienda;
+    }
+
+    public void setTipoVivienda(TipoViviendaDAO TipoVivienda) {
+        this.TipoVivienda = TipoVivienda;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+
+    public int getMunicipioID() {
+        return MunicipioID;
+    }
+
+    public void setMunicipioID(int MunicipioID) {
+        this.MunicipioID = MunicipioID;
+    }
+
+    public MunicipioDAO getMunicipio() {
+        return Municipio;
+    }
+
+    public void setMunicipio(MunicipioDAO Municipio) {
+        this.Municipio = Municipio;
+    }
+    
+    
+    
+}
