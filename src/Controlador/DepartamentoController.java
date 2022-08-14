1d9669bc96462d7b9f30376088340bb5374fe38b
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2022-08-13 23:56:48 -0500

    update y delete departamento mas documentación

--- src/Controlador/DepartamentoController.java
+++ src/Controlador/DepartamentoController.java
@@ -25,23 +25,23 @@ public class DepartamentoController extends Conexion {
      * @throws java.sql.SQLException
      */
     public int Create(DepartamentoDAO departamento) throws SQLException {
-        //crear la consulta
         String sql = "insert into Departamento(Nombre) values(?)";
-        //prepara la consulta
         PreparedStatement oPreparedStatement = conecta().prepareStatement(sql);
-        //ingresamos los parametros
         oPreparedStatement.setString(1, departamento.getNombre());
-
         return oPreparedStatement.executeUpdate();
 
     }
 
+    /**
+     * Obtiene una lista de departamentos
+     *
+     * @return
+     * @throws java.sql.SQLException
+     */
     public ArrayList<DepartamentoDAO> Get() throws SQLException {
         ArrayList<DepartamentoDAO> departamentosList = new ArrayList<>();
         String query = "SELECT * FROM Departamento order by Nombre";
-
         Statement st = conecta().createStatement();
-
         ResultSet rs = st.executeQuery(query);
         while (rs.next()) {
             departamentosList.add(new DepartamentoDAO(rs.getInt("ID"), rs.getString("Nombre")));
@@ -52,30 +52,54 @@ public class DepartamentoController extends Conexion {
 
     }
 
+    /**
+     * Obtiene un departamento por ID
+     *
+     * @param ID
+     * @return
+     * @throws java.sql.SQLException
+     */
     public DepartamentoDAO Get(int ID) throws SQLException {
         DepartamentoDAO departamento = new DepartamentoDAO();
-        String query = "SELECT * FROM Departamento WHERE ID = "+ID;
-
+        String query = "SELECT * FROM Departamento WHERE ID = " + ID;
         Statement st = conecta().createStatement();
-
         ResultSet rs = st.executeQuery(query);
-
         if (rs.next()) {
             departamento = new DepartamentoDAO(rs.getInt("ID"), rs.getString("Nombre"));
         }
-
         return departamento;
 
     }
 
-    public int Update() {
-        return 0;
+    /**
+     * Actualiza un departamento por ID
+     *
+     * @param ID
+     * @param departamento
+     * @return
+     * @throws java.sql.SQLException
+     */
+    public int Update(int ID, DepartamentoDAO departamento) throws SQLException {
+        String sql = "UPDATE Departamento SET  Nombre = ? WHERE ID = ?";
+        PreparedStatement oPreparedStatement = conecta().prepareStatement(sql);
+        oPreparedStatement.setString(1, departamento.getNombre());
+        oPreparedStatement.setInt(2, ID);
+        return oPreparedStatement.executeUpdate();
 
     }
 
-    public int Delete(int ID) {
-        return 0;
+    /**
+     * Eliminar un departamento por ID sino hay municipio registrado con ese departamento
+     * @param ID
+     * @return
+     * @throws java.sql.SQLException
+     * @throws SQLException*
+     */
+    public int Delete(int ID) throws SQLException {
+        String sql = "DELETE FROM Departamento WHERE ID = ?";
+        PreparedStatement oPreparedStatement = conecta().prepareStatement(sql);
+        oPreparedStatement.setInt(1, ID);
+        return oPreparedStatement.executeUpdate();
     }
 
-    
 }
